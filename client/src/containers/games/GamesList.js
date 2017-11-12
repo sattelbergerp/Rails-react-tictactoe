27317c730353492fff43570d8fdb0ec002145405
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-11-11 22:52:54 -0500

    Overhaul game list screen (change to table, add loading circle)

--- client/src/containers/games/GamesList.js
+++ client/src/containers/games/GamesList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchGames } from './../../actions/gamesActions';
 import { joinGame } from './../../actions/gameActions';
+import LoadingOverlay from './../../components/LoadingOverlay'
 
 class GamesList extends Component{
 
@@ -16,15 +17,28 @@ class GamesList extends Component{
   }
 
   render(){
-    return (<div>
-        <p>Is loading games list: {this.props.loading? "yes" : "no"}</p>
-        <ul>{this.props.games.map((game, index)=>{
-          return (<li>
-            Name: {game.name}, Started: {game.playing? "Yes" : "No"},
-            <Link to={'/games/'+game.id}>[Spectate]</Link>,
-            <a href="javascript:void(0)" onClick={event=>this.handleOnJoin(event, game.id)}>[Play]</a>
-            </li>);
-        })}</ul>
+    let gameList = this.props.games.map((game, index)=>{
+    return (<tr>
+      <td>{game.name}</td>
+      <td>{game.player1? game.player1.email : ""}</td>
+      <td>{game.player2? game.player2.email : ""}</td>
+      </tr>);
+    });
+
+    return (<div className="game-list-container">
+        <LoadingOverlay show={this.props.loading} bg="true" />
+        <table className="table">
+          <thead>
+            <tr>
+             <th scope="col">Game Name</th>
+             <th scope="col">Player 1</th>
+             <th scope="col">Player 2</th>
+           </tr>
+          </thead>
+          <tbody>
+            {gameList}
+          </tbody>
+        </table>
       </div>);
   }
 
