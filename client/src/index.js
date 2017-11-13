b4d900f1ad641b6ed2e6fde16e9530a044e87b8e
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-11-12 22:33:43 -0500

    Add redux dev tools extension suppor

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/index';
 import thunk from 'redux-thunk';
@@ -9,7 +9,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,
   document.getElementById('root'));
