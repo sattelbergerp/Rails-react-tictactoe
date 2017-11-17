33cd6f2c854ac0fa49fd8db74eae983177b2a738
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-11-16 22:11:07 -0500

    Add /users to navbar as Players

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -37,6 +37,7 @@ class NavBar extends Component {
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
               <li><NavLink to="/">Games<span className="sr-only"></span></NavLink></li>
+              <li><NavLink to="/users">Players<span className="sr-only"></span></NavLink></li>
               <li><NavLink to="/about">About<span className="sr-only"></span></NavLink></li>
             </ul>
             {accountLinks}
